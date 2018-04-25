a5cad0d35ad9c31fcfa08866c3d6f9131d308f43
Kayleigh Kernaghan <26681805+AylaJK@users.noreply.github.com>
2018-04-25 16:25:43 -0600

    Updating to CSS Modules
    Kept everything as CSS, but it is trival to switch to SASS when needed.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,26 +1,27 @@
 import React from "react";
 import {SocialSet} from "./SocialSet";
+import style from "./Header.css";
 export class Header  extends React.Component {
     render() {
         let button;
         if(this.props.user.name === "null"){
             button =
                 <div>
-                    <button type="button" className="btn btn-outline-success mar-right">Login</button>
+                    <button type="button" className={style.loginBtn}>Login</button>
                 </div>
         }else{
             button =
-                <div className="profile">
-                    <div className="profile-name">
+                <div className={style.profile}>
+                    <div className={style.name}>
                         {this.props.user.name}
                     </div>
-                    <span className="profile-icon fa fa-user"/>
+                    <span className={style.icon}/>
                 </div>
         }
         return(
-            <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+            <nav className={style.navbar}>
                 <SocialSet/>
-                <div className="navbar-brand text-center flex-fill justify-content-center">Hackbox Games</div>
+                <div className={style.branding}>Hackbox Games</div>
                 {button}
             </nav>
         )};
