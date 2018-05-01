1f7b8bef0a48d3be8a3d9516f35475869e4de647
Kayleigh Kernaghan <26681805+AylaJK@users.noreply.github.com>
2018-05-01 12:09:18 -0600

    Moving Server Connection Tests to Console

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const path = require('path');
 
-const http = require('http').Server(app);
+const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 const port = process.env.PORT || 8000;
@@ -18,14 +18,10 @@ app.use(cookieParser('secret'));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'client/build/index.html')));
 
-app.get('/hello', (req, res) => res.send({ express: 'Hello From the Express Server' }));
+app.get('/hello', (req, res) => res.send({ express: 'Hello From the Server REST API' }));
 
 io.on('connection', function(socket) {
-  socket.on('subscribeToTimer', interval => {
-    setInterval(() => {
-      socket.emit('timer', new Date());
-    }, interval);
-  });
+  socket.on('hello', () => socket.emit('hello', { socketIo: 'Hello From the Server Websocket API' }));
 });
 
 process.on('unhandledRejection', (r, p) => console.log("Possibly Unhandled Rejection at: Promise ", p, " reason: ", r));
