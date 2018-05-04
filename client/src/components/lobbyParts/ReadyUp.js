309fafc1708fa80ef5f2fc1bb2da7e71f7974961
Austin Wattling <Austin Wattling@CINDY>
2018-05-04 16:18:36 -0600

    Lobby loading I'm not super happy with and will probably change

---
+++ client/src/components/lobbyParts/ReadyUp.js
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./Lobby.css";
+export const ReadyUp = () => {
+    return (
+        <div className={style.ready}>
+            <div className={style.fields}>
+                <input type="text" className={"form-control"} id={"nickName"} placeholder={"Select a Nickname"} autoFocus/>
+            </div>
+            <button className={"btn btn-success"}> Ready up</button>
+        </div>
+    );
+};
